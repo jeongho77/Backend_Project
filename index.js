0298b3a18e861d9edb961f4404e082d805c264cb
minso00 <promingi7651@naver.com>
2023-05-26 11:01:00 +0900

    nav태그 클릭시 이동

---
+++ index.js
@@ -0,0 +1,12 @@
+//네비게이션 버튼 클릭시 화면 이동
+function moveCommunity() {
+    location.href = "#"
+}
+
+function moveKnowledge() {
+    location.href = "#"
+}
+
+function moveQ() {
+    location.href = "#"
+}
